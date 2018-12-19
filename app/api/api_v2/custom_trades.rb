7f7c7fe497089a2084301f66ec79e40c92bbb349
systemturcoindex <hasandenli@Hasans-MacBook-Pro.local>
2018-12-19 23:41:21 +0300

    update custom api with left join

--- app/api/api_v2/custom_trades.rb
+++ app/api/api_v2/custom_trades.rb
@@ -7,7 +7,8 @@ module APIv2
       use :market, :trade_filters
     end
     get "/custom-trades" do
-      trades = Trade.filter(params[:market], time_to, params[:from], params[:to], params[:limit], order_param)
+      # trades = Trade.filter(params[:market], time_to, params[:from], params[:to], params[:limit], order_param)
+      trades = Trade.joins('LEFT JOIN members as ask_member on trades.ask_member_id = ask_member.id').joins('LEFT JOIN members as bid_member on trades.bid_member_id = bid_member.id').filter(params[:market], time_to, params[:from], params[:to], params[:limit], order_param).select('trades.*,ask_member.email as ask_member_email, bid_member.email as bid_member_email')
       present trades, with: APIv2::Entities::CustomTrade
     end
   end
