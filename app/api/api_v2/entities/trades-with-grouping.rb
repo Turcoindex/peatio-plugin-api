787f4077973f1ffece34bbaf9ec7dcd19e5a0cdf
systemturcoindex <hasandenli@Hasans-MacBook-Pro.local>
2018-12-20 13:44:37 +0300

    test

---
+++ app/api/api_v2/entities/trades-with-grouping.rb
@@ -0,0 +1,13 @@
+# encoding: UTF-8
+# frozen_string_literal: true
+
+module APIv2
+    module Entities
+      class TradeWithGrouping < Base
+        expose :id
+        expose :email
+        expose :volume
+      end
+    end
+  end
+  
